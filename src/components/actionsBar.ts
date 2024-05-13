9e707814d380d2ff235cb458c57f7fdf36c3e579
André Dias <andredias.us@gmail.com>
2024-05-13 09:31:42 -0300

    Add simple text based logo

--- src/components/actionsBar.ts
+++ src/components/actionsBar.ts
@@ -4,6 +4,7 @@ function ActionsBar(): m.Component {
   return {
     view: function () {
       return m('div.actions-bar', [
+        m('h1.logo', 'My-Gym-Log'),
         m('button.btn-light-outline', 'Add Workout'),
       ]);
     },
