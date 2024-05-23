465dc0ac6f3d473836163e2acc28133d8d9e0855
André Dias <andredias.us@gmail.com>
2024-05-23 08:33:58 -0300

    Add inputs for workout modal

--- src/components/workoutModal.ts
+++ src/components/workoutModal.ts
@@ -3,7 +3,22 @@ import m from 'mithril';
 function WorkoutModal(): m.Component {
   return {
     view: function () {
-      return m('.workout-modal-content', 'Wokout Modal');
+      return m('form.workout-modal-content', [
+        m('h1', 'New Workout'),
+        m('div.input-wrapper', [
+          m('label', { for: 'date' }, 'Date'),
+          m('input[type=date]', { id: 'date', required: true }, 'New Workout'),
+        ]),
+        m('div.input-wrapper', [
+          m('label', { for: 'duration' }, 'Duration (min)'),
+          m('input[type=number]', {
+            id: 'duration',
+            inputmode: 'numeric',
+            pattern: 'd*',
+            required: true,
+          }),
+        ]),
+      ]);
     },
   };
 }
